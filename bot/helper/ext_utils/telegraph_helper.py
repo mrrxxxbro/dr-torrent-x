ac62a5e184b848e06e1431e5fcd2559d9fe54cd2
mrrxxxbro <127326036+mrrxxxbro@users.noreply.github.com>
2023-06-01 08:03:56 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -102,5 +102,5 @@ class TelegraphHelper:
 
 
 telegraph = TelegraphHelper(
-    'Z-Mirror', 'https://github.com/Dawn-India/Z-Mirror')
+    'Dr. Torrent X ', 'https://telegram.dog/drtorrentxupdates')
 bot_loop.run_until_complete(telegraph.create_account())
